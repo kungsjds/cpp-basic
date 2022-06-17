a94d768e7e427033039f7d51ab2ba02c1f9f2c38
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-06-17 14:39:04 -0300

    alloc_vector changes

--- alloc_vector.cpp
+++ alloc_vector.cpp
@@ -1,30 +1,32 @@
-//#include <iostream>
-//#include <new>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	int size;
-//
-//	cout << "Enter a vetor size: ";
-//	cin >> size;
-//
-//	// Create a pointer for the vector
-//	int *vect = new int[size];
-//
-//	for (int i = 0; i < size; i++) {
-//
-//		cout << "Enter a vector value in position " << i << ": ";
-//		cin >> vect[i];
-//
-//	}
-//	
-//
-//	for (int i = 0; i < size; i++) {
-//
-//		cout << "\nVector[" << i << "] = " << vect[i];
-//
-//	}
-//
-//}
+#include <iostream>
+#include <new>
+
+using namespace std;
+
+int main()
+{
+	int size;
+
+	cout << "Enter a vetor size: ";
+	cin >> size;
+
+	// Create a pointer for the vector
+	int *vect = new int[size];
+
+	for (int i = 0; i < size; i++) {
+
+		cout << "Enter a vector value in position " << i << ": ";
+		cin >> vect[i];
+
+	}
+	
+
+	for (int i = 0; i < size; i++) {
+
+		cout << "\nVector[" << i << "] = " << vect[i];
+
+	}
+
+	return 0;
+
+}
