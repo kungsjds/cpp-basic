cbe1a2a9db74d44a5c615d0a6d77196ebbd6c4d5
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-06-20 14:26:19 -0300

    Add class.

---
+++ class.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std; // To use cout/cin
+
+class Player { // Defines the class called Player
+
+public: // Defines public attributes
+	int level; // Attributes
+	float power;
+	string name;
+
+};
+
+int main()
+{
+
+	Player player1; // Create a player class object
+
+	cout << "Enter a player name: ";
+	cin >> player1.name;
+
+	cout << "\nEnter a player level: ";
+	cin >> player1.level;
+
+	cout << "\nEnter a player power: ";
+	cin >> player1.power;
+
+	cout << "\nName: " << player1.name;
+	cout << "\nLevel: " << player1.level;
+	cout << "\nPower: " << player1.power;
+
+}
