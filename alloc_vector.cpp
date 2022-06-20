aa00d6792eb1af32974d1e7ac5932e2d9213f6e9
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-06-20 09:01:27 -0300

    Add alloc_vector.

--- alloc_vector.cpp
+++ alloc_vector.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <new> // Used to allocate memory
-
-using namespace std;
-
-int main()
-{
-	int size;
-
-	cout << "Enter a vetor size: ";
-	cin >> size;
-
-	// Create a pointer for the vector
-	int *vect = new int[size];
-
-	for (int i = 0; i < size; i++) {
-
-		cout << "Enter a vector value in position " << i << ": ";
-		cin >> vect[i];
-
-	}
-	
-
-	for (int i = 0; i < size; i++) {
-
-		cout << "\nVector[" << i << "] = " << vect[i];
-
-	}
-
-	return 0;
-
-}
+//#include <iostream>
+//#include <new> // Used to allocate memory
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int size;
+//
+//	cout << "Enter a vetor size: ";
+//	cin >> size;
+//
+//	// Create a pointer for the vector
+//	int *vect = new int[size];
+//
+//	for (int i = 0; i < size; i++) {
+//
+//		cout << "Enter a vector value in position " << i << ": ";
+//		cin >> vect[i];
+//
+//	}
+//	
+//
+//	for (int i = 0; i < size; i++) {
+//
+//		cout << "\nVector[" << i << "] = " << vect[i];
+//
+//	}
+//
+//	return 0;
+//
+//}
